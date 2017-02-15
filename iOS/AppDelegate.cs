5da501e193f1aab44a3dfcdb0b99beb63c177554
Paul O Donnell <podonne5@gmail.com>
2017-02-15 13:11:09 +0000

    iOS receiving push notifications from firebase console Run app and use debug printed device token to send messages from firebase console

--- iOS/AppDelegate.cs
+++ iOS/AppDelegate.cs
@@ -9,11 +9,14 @@ using Newtonsoft.Json;
 using UIKit;
 
 using Facebook.CoreKit;
+using UserNotifications;
+using Firebase.CloudMessaging;
+using Firebase.InstanceID;
 
 namespace BigSlickChat.iOS
 {
 	[Register("AppDelegate")]
-	public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
+	public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate, IUNUserNotificationCenterDelegate, IMessagingDelegate
 	{        
         public override bool FinishedLaunching(UIApplication app, NSDictionary options)
 		{            
@@ -25,16 +28,93 @@ namespace BigSlickChat.iOS
             //removing seeing as it doesnt seem to be needed for anything
             //Facebook.CoreKit.ApplicationDelegate.SharedInstance.FinishedLaunching(app, options);
 
+            SetupFirebasePushNotifications(app);
+
 			LoadApplication(new App());
             		
 			return base.FinishedLaunching(app, options);
 		}
 
-        public override bool OpenUrl(UIApplication application, NSUrl url, string sourceApplication, NSObject annotation)
+        void SetupFirebasePushNotifications(UIApplication app)
         {
-            // We need to handle URLs by passing them to their own OpenUrl in order to make the SSO authentication works.
-            return ApplicationDelegate.SharedInstance.OpenUrl(application, url, sourceApplication, annotation);
+            InstanceId.Notifications.ObserveTokenRefresh((object sender, NSNotificationEventArgs e) =>
+            {                
+                Debug.WriteLine("DEVICE MESSAGING TOKEN\n" + InstanceId.SharedInstance.Token);
+
+                Messaging.SharedInstance.Connect(error =>
+                {
+                    if (error != null)
+                    {
+                        // Handle if something went wrong while connecting
+                        Debug.WriteLine("MESSAGING CONNECT ERROR " + error.Description);
+                    }
+                    else
+                    {
+                        // Let the user know that connection was successful
+                        Debug.WriteLine("MESSAGING CONNECT SUCCESS");
+                    }
+                });
+            });
+
+            if (UIDevice.CurrentDevice.CheckSystemVersion(10, 0))
+            {
+                //iOS 10 or later
+                UNAuthorizationOptions authOptions = UNAuthorizationOptions.Alert | UNAuthorizationOptions.Badge | UNAuthorizationOptions.Sound;
+
+                UNUserNotificationCenter.Current.RequestAuthorization(authOptions, (granted, error) =>
+                {
+                    Console.WriteLine(granted);
+                });
+
+                // For iOS 10 display notification (sent via APNS)
+                UNUserNotificationCenter.Current.Delegate = this;
+
+                // For iOS 10 data message (sent via FCM)
+                Messaging.SharedInstance.RemoteMessageDelegate = this;
+            }
+            else
+            {
+                // iOS 9 or before
+                UIUserNotificationType allNotificationTypes = UIUserNotificationType.Alert | UIUserNotificationType.Badge | UIUserNotificationType.Sound;
+                UIUserNotificationSettings settings = UIUserNotificationSettings.GetSettingsForTypes(allNotificationTypes, null);
+                UIApplication.SharedApplication.RegisterUserNotificationSettings(settings);
+            }
+
+            UIApplication.SharedApplication.RegisterForRemoteNotifications();
+
+            Debug.WriteLine("DEVICE MESSAGING TOKEN\n" + InstanceId.SharedInstance.Token);
         }
 
-	}
+        public override void DidReceiveRemoteNotification(UIApplication application, NSDictionary userInfo, Action<UIBackgroundFetchResult> completionHandler)
+        {
+            //Use this method to prep your app for when a user clicks on your notification
+            //Note: there is a known bug where this isnt called in iOS 10.0 it was fixed in 10.1
+        }
+
+        public void ApplicationReceivedRemoteMessage(RemoteMessage remoteMessage)
+        {
+            //This is needed to allow Firebase to forward messages received whilst in the foreground on iOS 10
+        }
+
+        // To receive notifications in foreground on iOS 10 devices.
+        [Export("userNotificationCenter:willPresentNotification:withCompletionHandler:")]
+        public void WillPresentNotification(UNUserNotificationCenter center, UNNotification notification, Action<UNNotificationPresentationOptions> completionHandler)
+        {
+            //present the notification as an alert even when received whilst app is in foreground
+            completionHandler(UNNotificationPresentationOptions.Alert);
+        }
+
+        public override void DidEnterBackground(UIApplication uiApplication)
+        {
+            Messaging.SharedInstance.Disconnect();
+
+            base.DidEnterBackground(uiApplication);
+        }
+
+        public override bool OpenUrl(UIApplication application, NSUrl url, string sourceApplication, NSObject annotation)
+        {
+            // We need to handle URLs by passing them to their own OpenUrl in order to make the SSO authentication work.
+            return ApplicationDelegate.SharedInstance.OpenUrl(application, url, sourceApplication, annotation);
+        }
+    }
 }
