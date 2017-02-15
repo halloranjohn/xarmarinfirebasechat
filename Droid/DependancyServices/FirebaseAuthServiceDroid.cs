b0f4d995aba1366615b722991ac9ee4ad103d70d
Paul O Donnell <podonne5@gmail.com>
2017-02-15 17:41:31 +0000

    fixes for android auth callbacks

--- Droid/DependancyServices/FirebaseAuthServiceDroid.cs
+++ Droid/DependancyServices/FirebaseAuthServiceDroid.cs
@@ -7,101 +7,113 @@ using Firebase.Auth;
 using Firebase.Iid;
 using Java.Lang;
 using Xamarin.Forms;
+using Android.App;
 
 [assembly: Dependency(typeof(BigSlickChat.Droid.FirebaseAuthServiceDroid))]
 namespace BigSlickChat.Droid
 {
-	public class FirebaseAuthServiceDroid : FirebaseAuthService
-	{
-		public static MainActivity mainActivity;
+    public class FirebaseAuthServiceDroid : FirebaseAuthService
+    {
+        private Activity CurrentActivity
+        {
+            get
+            {
+                return Forms.Context as Activity;
+            }
+        }
 
-		public void CreateUser(string username, string password, Action onCompleteAction, Action<string> onErrorAction)
-		{
-			if (FirebaseAuth.Instance.CurrentUser == null)
-			{
-				FirebaseAuth.Instance.CreateUserWithEmailAndPassword(username, password)
-							.AddOnFailureListener(mainActivity, new FailureListener(onErrorAction))
-							.AddOnCompleteListener(mainActivity, new CompleteListener())
-							.AddOnSuccessListener(mainActivity, new SuccessListener(onCompleteAction));
-			}
-			else
-			{
-				string uid = FirebaseAuth.Instance.CurrentUser.Uid;
-			}
-		}
+        public void CreateUser(string username, string password, Action onCompleteAction, Action<string> onErrorAction)
+        {
+            if (FirebaseAuth.Instance.CurrentUser == null)
+            {
+                FirebaseAuth.Instance.CreateUserWithEmailAndPassword(username, password)
+                            .AddOnFailureListener(CurrentActivity, new FailureListener(onErrorAction))
+                            .AddOnSuccessListener(CurrentActivity, new SuccessListener(onCompleteAction));
+            }
+            else
+            {
+                string uid = FirebaseAuth.Instance.CurrentUser.Uid;
+            }
+        }
 
-		public void SignOut()
-		{
-			Log.Debug("BIGSLICKAPP", "InstanceID token: " + FirebaseInstanceId.Instance.Token);
+        public void SignOut()
+        {
+            Log.Debug("BIGSLICKAPP", "InstanceID token: " + FirebaseInstanceId.Instance.Token);
 
-			if (FirebaseAuth.Instance.CurrentUser != null)
-			{
-				FirebaseAuth.Instance.SignOut();
-			}
+            if (FirebaseAuth.Instance.CurrentUser != null)
+            {
+                FirebaseAuth.Instance.SignOut();
+            }
 
-			Log.Debug("BIGSLICKAPP", "InstanceID token: " + FirebaseInstanceId.Instance.Token);
-		}
+            Log.Debug("BIGSLICKAPP", "InstanceID token: " + FirebaseInstanceId.Instance.Token);
+        }
 
-		public void SignIn(string username, string password, Action onCompleteAction, Action<string> onErrorAction)
-		{
-			if (FirebaseAuth.Instance.CurrentUser == null)
-			{
-				FirebaseAuth.Instance.SignInWithEmailAndPassword(username, password);
-			}
-			else
-			{
-				onErrorAction("Already logged in as " + FirebaseAuth.Instance.CurrentUser.Email);
-			}
-		}
+        public void SignIn(string username, string password, Action onCompleteAction, Action<string> onErrorAction)
+        {
+            if (FirebaseAuth.Instance.CurrentUser == null)
+            {
+                FirebaseAuth.Instance.SignInWithEmailAndPassword(username, password)
+                            .AddOnSuccessListener(CurrentActivity, new SuccessListener(onCompleteAction))
+                            .AddOnFailureListener(CurrentActivity, new FailureListener(onErrorAction));
+            }
+            else
+            {
+                onErrorAction("Already logged in as " + FirebaseAuth.Instance.CurrentUser.Email);
+            }
+        }
 
-		public string GetUid()
-		{
-			if (FirebaseAuth.Instance.CurrentUser != null)
-			{
-				return FirebaseAuth.Instance.CurrentUser.Uid;
-			}
+        public string GetUid()
+        {
+            if (FirebaseAuth.Instance.CurrentUser != null)
+            {
+                return FirebaseAuth.Instance.CurrentUser.Uid;
+            }
 
-			return null;
-		}
-	}
+            return null;
+        }
+    }
 
-	public class FailureListener : Java.Lang.Object, IOnFailureListener
-	{
-		private Action<string> OnFailureAction;
+    public class FailureListener : Java.Lang.Object, IOnFailureListener
+    {
+        private Action<string> OnFailureAction;
 
-		public FailureListener(Action<string> onFailureAction)
-		{
-			OnFailureAction = onFailureAction;
-		}
+        public FailureListener(Action<string> onFailureAction)
+        {
+            OnFailureAction = onFailureAction;
+        }
 
-		void IOnFailureListener.OnFailure(Java.Lang.Exception e)
-		{
-			Debug.WriteLine("FailureListener : " + e.Message);
-			OnFailureAction(e.Message);
-		}
-	}
+        void IOnFailureListener.OnFailure(Java.Lang.Exception e)
+        {
+            Debug.WriteLine("FailureListener : " + e.Message);
 
-	public class CompleteListener : Java.Lang.Object, IOnCompleteListener
-	{
-		void IOnCompleteListener.OnComplete(Task task)
-		{
-			Debug.WriteLine("CompleteListener : ");
-		}
-	}
+            if (OnFailureAction != null)
+                OnFailureAction(e.Message);
+        }
+    }
 
-	public class SuccessListener : Java.Lang.Object, IOnSuccessListener
-	{
-		private Action OnSuccessAction;
+    public class CompleteListener : Java.Lang.Object, IOnCompleteListener
+    {
+        void IOnCompleteListener.OnComplete(Task task)
+        {
+            Debug.WriteLine("CompleteListener : ");
+        }
+    }
 
-		public SuccessListener(Action onSuccessAction)
-		{
-			OnSuccessAction = onSuccessAction;
-		}
+    public class SuccessListener : Java.Lang.Object, IOnSuccessListener
+    {
+        private Action OnSuccessAction;
 
-		public void OnSuccess(Java.Lang.Object result)
-		{
-			Debug.WriteLine("SuccessListener : " + result.ToString());
-			OnSuccessAction();
-		}
-	}
+        public SuccessListener(Action onSuccessAction)
+        {
+            OnSuccessAction = onSuccessAction;
+        }
+
+        public void OnSuccess(Java.Lang.Object result)
+        {
+            Debug.WriteLine("SuccessListener : " + result.ToString());
+
+            if (OnSuccessAction != null)
+                OnSuccessAction();
+        }
+    }
 }
