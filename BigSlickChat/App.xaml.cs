ac0da3a3488530c442eb21cb673cde32d40ab587
Brian <shrinebotsligo@gmail.com>
2017-02-21 11:37:59 +0000

    OneSignal v3.0.0 working on ios device. Note: had to change to use brians Firebase onesignal app, as push wasnt setup for bigslickchatapp

--- BigSlickChat/App.xaml.cs
+++ BigSlickChat/App.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Xamarin.Forms;
 using Com.OneSignal;
+using System.Diagnostics;
 
 namespace BigSlickChat
 {
@@ -16,6 +17,9 @@ namespace BigSlickChat
 			//MainPage = new LoginPage();
 
 			OneSignal.Current.StartInit("02860e0a-f3ba-421c-9cb9-5c3a3d69d03e").EndInit();
+
+			DependencyService.Get<OneSignalService>().SendMessageToUser("95805d2f-3885-4360-8699-7dba6a244224",
+			                                                            "Test Message", () => { Debug.WriteLine("Success"); }, () => Debug.WriteLine("Failure"));
 		}
 
         void InitServices()
